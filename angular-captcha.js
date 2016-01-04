b06becaf5558236c17e772f7da8fc89fe3250874
Rajan G <rajan.g@npcompete.org>
2016-01-04 17:06:08 +0530

    audio support with detail file

--- angular-captcha.js
+++ angular-captcha.js
@@ -69,7 +69,17 @@
                 };
 
                 var speechText = function (text) {
-                  var u1 = new SpeechSynthesisUtterance(text);
+                  if ("speechSynthesis" in window) {
+                    console.log("browser supports speech synthesis.");
+                  } else {
+                    console.log("browser does NOT support speech sythesis")
+                    return;
+                  }
+
+                  var fallbackSpeechSynthesis = window.speechSynthesis || window.speechSynthesisPolyfill;
+                  var fallbackSpeechSynthesisUtterance = window.SpeechSynthesisUtterance || window.SpeechSynthesisUtterancePolyfill;
+                  //var u1 = new SpeechSynthesisUtterance(text);
+                  var u1 = new fallbackSpeechSynthesisUtterance(text);
                   u1.lang = 'en-US';
                   u1.pitch = 1;
 //                  u1.rate = 1;
@@ -77,33 +87,39 @@
                   //u1.voice = voices[10];
                   u1.voiceURI = 'native';
                   u1.volume = 3;
-                  speechSynthesis.speak(u1);
+                  //speechSynthesis.speak(u1);
+                  fallbackSpeechSynthesis.speak(u1);
                 };
+				
+				var speech = function(src) {
+					var index = 1;
+					var audio = new Audio(src[0]);
+					audio.play();
+					audio.onended = function() {
+						if(index <src.length) {
+							audio.src = src[index];
+							audio.play();
+							index++
+							}
+					}
+				}
                 scope.speech = function () {
+				  var srcArray = [];
                   for (var i = 0; i < scope.captchaText.length; i++) {
-                    speechText(scope.captchaText[i]);
+					  srcArray.push('sound/en-us/'+scope.captchaText[i].toLowerCase()+'.mp3');                    				
                   }
+				  speech(srcArray);
                 };
+
                 scope.change = function () {
                   initCaptcha();
                 };
-                /* var drawTextIn3D = function(ctx, formatedText,width, height,depth) {
-                 var cnt;
-                 for(cnt=0; cnt< depth; cnt++) {
-                 //ctx.fillText(formatedText, width-cnt, height-cnt);
-                 }
-                 ctx.shadowColor="#000FFF"
-                 ctx.shadowBlur = 8;
-                 ctx.shadowOffsetX = depth+2;
-                 ctx.shadowOffsetY = depth+2;
-                 ctx.fillText(formatedText,0, 25);
-                 
-                 };
-                 */
+
                 var initCaptcha = function () {
                   var expected = attrs['expected'];
                   var captchaText = captchaTextProvider(expected);
                   element[0].childNodes[0].childNodes[0].src = textToImg(captchaText);
+                  element[0].childNodes[0].className  = 'captcha-container bg'+Math.floor(Math.random()*9);
                   var matcher = attrs['captchamatcher'];
                   scope.$parent[matcher] = captchaText;
                   scope.captchaText = captchaText;
@@ -119,3 +135,5 @@
           });
 
 }());
+
+//http://lea.verou.me/css3patterns/
