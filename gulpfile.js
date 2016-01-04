cd364bd11c08645b9f04ae1cda744c6d338b0cce
Rajan G <rajan.g@npcompete.org>
2016-01-04 21:10:02 +0530

    minification completed

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,36 @@
  * and open the template in the editor.
  */
 
-var gulp = require('gulp');
+var gulp 	= require('gulp');
+var uglify  = require('gulp-uglify');
+var del 	= require('del');
+var concat	= require('gulp-concat');
+var notify  = require('gulp-notify');
+var minifyCss = require('gulp-minify-css');
 
-gulp.task('default', function () {
-  // place code for your default task here
+
+gulp.task('clean:dest', function () {
+    return del([
+        'dist/*',
+    ]);
 });
+
+gulp.task('minify-js', function () {
+    gulp.src([
+        'angular-captcha.js',
+    ])       
+	.pipe(uglify({mangle: false}))
+	.pipe(concat('angular-captcha-min.js'))
+	.on("error", notify.onError(function (error) {
+		return "Error: " + error.message;
+        }))
+    .pipe(gulp.dest('dist/'))
+});
+
+gulp.task('minify-css', function() {
+  return gulp.src('style/*.css')
+    .pipe(minifyCss({compatibility: 'ie8'}))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default',['clean:dest', 'minify-js', 'minify-css']);
